a9d57c04a27d94216beff1397ef24c83fc540566
Boris Smetanin <boris.smetanin1703@gmail.com>
2018-10-06 23:57:48 +0300

    1.0.0   refactor front end (#1)
    * 1.0.0 - refacored front end

--- wp-content/themes/hivrot/header.php
+++ wp-content/themes/hivrot/header.php
@@ -15,14 +15,16 @@
     <!-- assets main root wp-content/themes/hivrot/assets -->
     <!-- Custom fonts for this template -->
     <link href="<?= get_option('siteurl') ?>/wp-content/themes/hivrot/assets/lib/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <!-- <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css"> -->
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Heebo" rel="stylesheet">
 
     <!-- Plugin CSS -->
     <link href="<?= get_option('siteurl') ?>/wp-content/themes/hivrot/assets/lib/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
 
-    <!-- Custom styles for this template -->
+    <!-- Custom styles for freelancer template -->
     <link href="<?= get_option('siteurl') ?>/wp-content/themes/hivrot/assets/css/freelancer.css" rel="stylesheet">
+    <!--  Custom styles for hivrote (this site) template -->
+    <link href="<?= get_option('siteurl') ?>/wp-content/themes/hivrot/style.css" rel="stylesheet">
+    <!-- TODO use wp_head() in here-->
 
   </head>
 
@@ -38,15 +40,17 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav">
-            <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">קיראו עוד</a>
-            </li>
-            <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">קצת עלינו</a>
-            </li>
-            <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">צורו קשר</a>
-            </li>
+              <?php
+                foreach(get_hivrot_page_link_items() as $hivrote_page_link_item) {
+              ?>
+                    <li class="nav-item mx-0 mx-lg-1">
+                        <a class="nav-link py-3 px-0 px-lg-3 rounded <?= isset($hivrote_page_link_item['class']) ?$hivrote_page_link_item['class'] : '' ?>" href="<?= $hivrote_page_link_item['link_herf']?>">
+                            <?= $hivrote_page_link_item['title']?>
+                        </a>
+                    </li>
+              <?php
+                }
+              ?>
           </ul>
         </div>
 
--- wp-content/themes/hivrot/index.php
+++ wp-content/themes/hivrot/index.php
@@ -1,35 +1,116 @@
 <?php
 get_header();
+$hivrot_page = get_post();
+if (is_front_page()){
 ?>
+
+ <section class="bg-primary text-white mb-0 hivrot-page" id="about">
+      <div class="container">
+        <h2 class="text-center text-uppercase text-white"><?=$hivrot_page->post_title?></h2>
+        <hr class="star-light mb-5">
+        <div class="row">
+            <div class="col-md-12 col-lg-12 hivrot-page-content">
+                <?= apply_filters('the_content', $hivrot_page->post_content); ?>
+            </div>
+        </div>
+      </div>
+  </section>
+
  <!-- Portfolio Grid Section -->
  <section class="portfolio" id="portfolio">
-      <div class="container">
-        <h2 class="text-center text-uppercase text-secondary mb-0">Portfolio</h2>
+    <div class="container">
+        <h2 class="text-center text-uppercase text-secondary mb-0">קיראו עוד</h2>
         <hr class="star-dark mb-5">
 
         <div class="row">
           <?php
-            foreach(get_hivrot_menue_pages() as $page_obj){
+            foreach(get_hivrot_page_items() as $hivrot_page_item){
           ?>
               <div class="col-md-6 col-lg-4">
-                <a class="portfolio-item d-block mx-auto" href="<?=get_option('siteurl').'/'.$page_obj->post_name ?>">
-                  <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
-                    <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
-                      <i class="fas fa-search-plus fa-3x"></i>
-                    </div>
-                  </div>
-                  <h2><?=$page_obj->post_title?> </h2>
-                  <!-- <img class="img-fluid" src="wp-content/themes/hivrot/assets/img/portfolio/cabin.png" alt=""> -->
+                <a class="hivrot-page-item d-block mx-auto" href="<?=$hivrot_page_item['page_url'] ?>">
+                  <h2><?=$hivrot_page_item['post_title']?> </h2>
+                  <p class="page-post"><?= $hivrot_page_item['page_trimmed_content']?> <span class="read-more bold">(קיראו עוד)</span> </p>
                 </a>
               </div>
 
           <?php
             }
           ?>
-
-      </div>
+          <!-- end of row -->
+        </div> 
+      <!-- end of portfolio container -->
+    </div>
+    <!-- end of portfolio section -->
 </section>
 
+<section class="" id="activity-gallery">
+  <div class="container">
+    <h2 class="text-center text-uppercase">טעימות מהפעילות</h2>
+    <hr class="star-dark mb-5">
+    <div class="row">
+      <div class="col-md-12 col-lg-12">
+        <?php
+          $main_page_gallery = get_main_page_gallery();
+        ?>
+        <div id="hivrot-main-page-gallery" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+
+            <?php
+              foreach($main_page_gallery as $i => $main_page_gallery_image){
+            ?>
+              <li data-target="#hivrot-main-page-gallery" data-slide-to="0" <?= $i ===0 ? 'class="active"' : ''?>></li>
+            <?php
+              }
+            ?>
+          </ol>
+          <div class="carousel-inner">
+            <?php
+              foreach($main_page_gallery as $i => $main_page_gallery_image){
+            ?>
+            <div class="carousel-item <?= $i ===0 ? 'active' : ''?>">
+              <img class="d-block w-100" src="<?=$main_page_gallery_image['guid']?>" alt="<?=$main_page_gallery_image['post_excerpt']?>">
+            </div>
+          <?php
+              }
+            ?>
+          </div>
+          <a class="carousel-control-prev" href="#hivrot-main-page-gallery" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#hivrot-main-page-gallery" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+
+      </div><!-- End of column -->
+    
+    </div><!-- End of row -->
+        
+  </div><!-- End of container -->
+      
+</section><!-- End of section -->
+<?php
+} else {
+?>
+  <section class="hivrot-page" id="hivrot-page">
+      <div class="container">
+          <h2 class="text-center text-uppercase text-secondary mb-0"><?=$hivrot_page->post_title?></h2>
+          <hr class="star-dark mb-5">
+          <div class="row">
+            <div class="col-md-12 col-lg-12 hivrot-page-content">
+                <?= apply_filters('the_content', $hivrot_page->post_content); ?>
+            </div>
+
+        <!-- end of portfolio container -->
+      </div>
+      <!-- end of portfolio section -->
+  </section>
+
+<?php
+}
+?>
 <?php
 get_footer();
 ?>
